9edced91499e73fb5b32ec684dc7139e7ecadc43
Robert Bohdziewicz <github@bohdziewicz.com.pl>
2022-01-28 16:10:00 +0100

    First solution

--- bash/two-fer/two_fer.sh
+++ bash/two-fer/two_fer.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-if [ $# -eq 0 ]; then 
-	printf "One for you, one for me."
-else
-	echo "One for $1, one for me."
-fi
+######
+# 	if [ $# -eq 0 ]; then 
+#		printf "One for you, one for me."
+# 	else
+# 	echo "One for $1, one for me."
+#	fi
+######
+
+# And now take a look my young padawan 
+
+echo "One for ${1:-you}, one for me."
+
+
+
