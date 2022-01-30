32b5fd6ca9e3df2f3890d8ed05c64349bd86df11
Robert Bohdziewicz <github@bohdziewicz.com.pl>
2022-01-30 16:17:50 +0100

    Task: Error-Handling (easy)

--- bash/error-handling/error_handling.sh
+++ bash/error-handling/error_handling.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env bash
 
-set -o errexit
-set -o nounset
-
-main() {
-	case $# in
-		0) printf "%s" "Usage: error_handling.sh <person>"; return 1 ;;
-		1) printf "%s" "Hello, $1"; return 0 ;;
-		*) printf "%s" "Usage: error_handling.sh <person>"; return 1 ;;
-	esac
-}
-
-main "$@"
+### There wasn't any looking good solutions
